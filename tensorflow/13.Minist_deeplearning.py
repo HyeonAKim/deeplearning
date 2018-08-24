945602373803840fa5b64c2b9cde7ebe0d4fdaea
hyeona kim <>
2018-08-24 19:02:14 +0900

    deeplearning_tensorflow

---
+++ tensorflow/13.Minist_deeplearning.py
@@ -0,0 +1,141 @@
+"""
+activation funcation : 하나의 값이 다음 값으로 넘어갈때 임계치 이상이면 작동을 하고 아니면 작동을 하지 않도록 하는것
+
+1. Xsigmoid 보다 Relu가 좋아
+# 9단으로 연결을 해도 정확도가 높아지지 않음
+# Backpropagation > 깊이가 깊은 레이어는 학습이 잘되지 않음 왜? > 시그모이드 게이트를 통과해서 미분값이 너무 작아지는 문제
+# 1998 ~ 2006년도 까지 이문제를 해결하지 못함
+
+2. Weight초기화를 잘해보자.
+# 초기값을 굉장히 멍청하게 둠 > 왜? > 기존 : -1 ~ 1 사이의 랜덤값을 줌
+# 초기값을 0 으로 준다면 ? > 미분*0 이되어서 미분값이 다 0 이 되버림
+# 2006 : breaktrough > RBM을 사용 > Fine tunning
+# RBM을 사용하지 않아도 괜찮음 : Xavier , He's initialization
+# Glorot et al.2010
+W = np.random.randn(fan_in, fan_out)/ np.sqrt(fan_in)
+# He et al. 2015
+W = np.random.randn(fan_in, fan_out)/np.sqrt(fan_in/2)
+prettytensor implementaion - 사용
+
+3. drop out
+overfitting - train 데이터에 너무 학습된 상태 , 테스트 데이터에서 정확도가 낮게 나올경우
+- overfitting 해결 방법
+# train data 가 많아야함
+# reqularization 을 하자
+- 구부러진 함수를 피자
+- dropout 하자
+dropout_rate = tt.placeholder("float")
+_L1 = tf.nn.relu(tf.add(tf.matmul(X, W1), B1))
+L1 = tf.nn.dropout(_L1, dropout_rate)
+
+** 주의사항 : dropout은 학습할 때만 사용
+Train
+    sess.run(optimizer, feed_dict={X: batch_x, Y: batch_y, dropoutrate: 0.7}
+Evaluation
+    accuracy.eval({X: mnist.test.images, Y: mnist.test.labels, dropouttate: 1}
+
+4. 앙상블
+기계가 많고 학습 장비가 많을 경우  : 최소 2% ~ 4% 5% 까지 성능향상
+
+5. 레고처럼 네트워크를 쌓아보자.
+feedforward neural network.
+Fast forward : resnet 구조
+split and Merge : 입력을 나누워서 처리하고 하나로 모을 수 도 있음 ( convolutional network )
+Recurrent network : RNN
+The only limit is your imagination.
+
+6. optimizer 의 종류
+# simulation 페이지가 있음
+# 처음의 시작은 adam 으로 하자 .
+
+
+## summary
+1. softmax vs Neural Nets for MNIST, 90 and 95.4%
+2. Xavier initailization : 97.8%
+3. Deep Neural Nets with Dropout : 98%
+4. Adam and other optimizers
+5. Exercise: Batch Normalization
+
+"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 1
+# Input Data
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+
+# 2
+# Create layers : with relu
+# layer 5 : accuracy : 0.9764
+# dropout : train > 0.5 ~ 0.7 / test > 1: accuracy : 0.9795
+
+W1 = tf.get_variable("W1", shape=[784, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([256]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
+
+W2 = tf.get_variable("W2", shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([256]))
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+
+
+W3 = tf.get_variable("W3", shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([256]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+
+W4 = tf.get_variable("W4", shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([256]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+
+
+W5 = tf.get_variable("W5", shape=[256, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L4, W5) + b5
+
+
+# 3
+# Define cost, optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# 4
+# Initialize : xavier initalization tensorflow
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# 5
+# Train Model
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), "cost =", '{:.9f}'.format(avg_cost))
+
+print('Learning Finished')
+
+# 6
+# Test Model
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict= {X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
---
+++ tensorflow/15.Mnist_Batchnorm.py
@@ -0,0 +1,263 @@
+# 1
+# Load library and load data
+import tensorflow as tf
+# import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# print(mnist.train.images.shape)
+# ( 55000, 784 ) 784 사이즈의 55000건의 데이터가 있음을 말함
+
+# 2
+# Define Model and Solver Class
+# Object-Oriented-Programming allow to define multiple model easily.
+# Why do we separeate model and solver classes?
+# We can just swap out the model class in the solver class when we need a different network architecture.
+# Usually we need one solver class.
+
+class Model:
+    """Network Model class
+
+    Note that class has only the constructor.
+    The actual model is defined inside the constructor.
+
+    Attributes
+    ----------
+    X : tf.float32
+        This is a tensorflow placholder for MNIST images
+        Expected shape is (None, 784)
+
+    y : tf.float32
+        This is a tensorflow placeholder for MNIST labels
+        Expected shape is (None, 10)
+
+    mode : tf.bool
+        This is used for the batch normalization.
+        It's 'True' at training time and 'False' at test time.
+
+    loss : tf.float32
+        The loss function is a softmax cross entropy.
+
+    train_op
+        This is simply the training op that minimizes the loss.
+
+    accuracy : tf.float32
+        The accuracy operation
+
+    """
+    def __init__(self, name, input_dim, output_dim, hidden_dims=[32,32],
+                 use_batchnorm=True, activation_fn=tf.nn.relu,
+                 optimizer=tf.train.AdamOptimizer, lr= 0.01):
+        """ Constructor
+
+        :param name: str
+            The name of this network
+            The entire network will be created under 'tf.variable_scope(name)'
+
+        :param input_dim: int
+            The input dimension
+            In this example, 784
+
+        :param output_dim: int
+            The number of output labels
+            There are 10 labels
+
+        :param hidden_dims: List ( default:[32, 32])
+            len(hidden_dims) = number of layers
+            each element is the number of hidden units
+
+        :param use_batchnorm: bool ( default : True )
+            If true, it will create the batchmormaliztion Layer
+
+        :param activation_fn: TF Functions ( default: tf.nn.relu )
+            Activation Function
+
+        :param optimizer: TF optimizer ( default : tf.train.AdamOptimizer)
+            Optimizer Function
+
+        :param lr: float (default : 0.01)
+            Learning rate
+        """
+        with tf.variable_scope(name):
+            # placedholders are defined
+            self.X = tf.placeholder(tf.float32, [None, input_dim], name='X')
+            self.y = tf.placeholder(tf.float32, [None, output_dim], name='y')
+            self.mode = tf.placeholder(tf.bool, name='train_mode')
+
+            # Loop over hidden layers
+            net = self.X
+            for i, h_dim in enumerate(hidden_dims):
+                with tf.variable_scope('layer{}'.format(i)):
+                    net = tf.layers.dense(net, h_dim)
+
+                    if use_batchnorm:
+                        net = tf.layers.batch_normalization(net, training=self.mode)
+
+                    net = activation_fn(net)
+
+            # Attach fully connected layers
+            net = tf.contrib.layers.flatten(net)
+            net = tf.layers.dense(net, output_dim)
+
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=net, labels=self.y)
+            self.loss = tf.reduce_mean(self.loss, name='loss')
+
+            # When using the batchnormalization layers,
+            # it is necessary to manually add the update operations
+            # because the moving averages are not included in the graph
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=name)
+            with tf.control_dependencies(update_ops):
+                self.train_op = optimizer(lr).minimize(self.loss)
+
+            # Accuracy etc
+            softtmax = tf.nn.softmax(net, name='softmax')
+            self.accuracy = tf.equal(tf.argmax(softtmax, 1), tf.argmax(self.y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(self.accuracy, tf.float32))
+
+class Solver:
+    """Solver class
+
+    This class  will contain the model class and session
+
+    Attrivutes
+    ---------
+    model : Model class
+    sess : TF session
+
+    Methods
+    -------
+    train(X,y)
+        Run the train_op and Returns the loss
+
+    evaluate(X, y, batch_size=None)
+        Returns "Loss" and "Accuracy"
+        If batch_size is given, it's computed using batch_size
+        becaues most GPU memorices cannot handle entire training data at once
+
+
+    Example
+    -------
+    >>> sess = tf.InteractiveSession()
+    >>> model = Model("BatchNorm", 32, 10)
+    >>> solver = Solver(sess, model)
+
+    # Train
+    >>> solver.train(X, y)
+
+    # Evaluate
+    >>> solver.evaluate(X, y)
+    """
+
+    def __init__(self, sess, model):
+        self.model = model
+        self.sess = sess
+
+    def train(self, X, y):
+        feed = {
+            self.model.X: X,
+            self.model.y: y,
+            self.model.mode: True
+        }
+        train_op = self.model.train_op
+        loss = self.model.loss
+
+        return self.sess.run([train_op, loss], feed_dict=feed)
+
+    def evaluate(self, X, y, batch_size=None):
+        if batch_size:
+            N = X.shape[0]
+
+            total_loss = 0
+            total_acc = 0
+
+            for i in range(0, N, batch_size):
+                X_batch = X[i:i + batch_size]
+                y_batch = y[i:i + batch_size]
+
+                feed = {
+                    self.model.X: X_batch,
+                    self.model.y: y_batch,
+                    self.model.mode: False
+                }
+
+                loss = self.model.loss
+                accuracy = self.model.accuracy
+
+                step_loss, step_acc = self.sess.run([loss, accuracy],
+                                                    feed_dict=feed)
+                total_loss += step_loss * X_batch.shape[0]
+                total_acc += step_acc * X_batch.shape[0]
+
+            total_loss /= N
+            total_acc /= N
+
+            return total_loss, total_acc
+
+        else:
+            feed = {
+                self.model.X : X,
+                self.model.y : y,
+                self.model.mode: False
+            }
+            loss = self.model.loss
+            accuracy = self.model.accuracy
+
+            return self.sess.run([loss, accuracy], feed_dict=feed)
+
+# 3
+# Instantiate Model/Solver classes
+
+input_dim = 784
+output_dim = 10
+N = 55000
+
+tf.reset_default_graph()
+sess = tf.InteractiveSession()
+
+# We create two models : one with the batch norm and other withour
+bn = Model('batchnorm', input_dim, output_dim, use_batchnorm=True)
+nn = Model('no_norm', input_dim, output_dim, use_batchnorm=False)
+
+# We create two solvers: to train both models at the same time for comparison
+# Usuaaly we only need one solver class
+bn_solver = Solver(sess, bn)
+nn_solver = Solver(sess, nn)
+
+epoch_n = 10
+batch_size = 32
+
+# Save Lossess and Accuracies every epoch
+# We are going to plot them later
+train_losses = []
+train_accs = []
+
+valid_losses = []
+valid_accs = []
+
+# 4
+# Run the train step
+init = tf.global_variables_initializer()
+sess.run(init)
+
+for epoch in range(epoch_n):
+    for _ in range(int(N/batch_size)):
+        X_batch, y_batch = mnist.train.next_batch(batch_size)
+
+        _, bn_loss = bn_solver.train(X_batch, y_batch)
+        _, nn_loss = nn_solver.train(X_batch, y_batch)
+
+    b_loss, b_acc = bn_solver.evaluate(mnist.train.images, mnist.train.labels, batch_size)
+    n_loss, n_acc = nn_solver.evaluate(mnist.train.images, mnist.train.labels, batch_size)
+
+    # Save train losses / acc
+    train_losses.append([b_loss, n_loss])
+    train_accs.append([b_acc, n_acc])
+    print(f'[Epoch {epoch}-TRAIN] Batchnorm Loss(Acc): {b_loss:.5f}({b_acc:.2%}) vs No Batchnorm Loss(Acc): {n_loss:.5f}({n_acc:.2%})')
+
+    b_loss, b_acc = bn_solver.evaluate(mnist.validation.images, mnist.validation.labels)
+    n_loss, n_ass = nn_solver.evaluate(mnist.validation.images, mnist.validation.labels)
+
+    # Save valid lossed/acc
+    valid_losses.append([b_loss, n_loss])
+    valid_accs.append([b_acc, n_acc])
+    print(f'[Epoch {epoch}-VALID] Batchnorm Loss(Acc): {b_loss:.5f}({b_acc:.2%}) vs No Batchnorm Loss(Acc): {n_loss:.5f}({n_acc:.2%})')
